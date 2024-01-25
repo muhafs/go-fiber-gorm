61b3a87dc620022d5f10e8b6fe94231ad6f77a8b
Muhamad <muhamadhaspin@gmail.com>
2024-01-25 09:25:07 +0700

    separate configs into configs folder

---
+++ config/loader.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Env struct {
+	DBHost string `mapstructure:"DB_HOST"`
+	DBPort string `mapstructure:"DB_PORT"`
+	DBUser string `mapstructure:"DB_USER"`
+	DBPass string `mapstructure:"DB_PASS"`
+	DBName string `mapstructure:"DB_NAME"`
+
+	AppURL string `mapstructure:"APP_URL"`
+}
+
+func LoadEnv(path string) (env Env, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigType("env")
+	viper.SetConfigName("app")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&env)
+	return
+}
