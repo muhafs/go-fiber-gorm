be7adae38810c198a05d9dc2b8cd5f520694e793
Muhamad <muhamadhaspin@gmail.com>
2024-01-26 12:03:58 +0700

    add jwt utils for generating and verivying tokens

---
+++ utils/jwt.utils.go
@@ -0,0 +1,42 @@
+package utils
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/muhafs/go-fiber-gorm/model/entity"
+)
+
+var secretKey = []byte("secret-key")
+
+func CreateToken(user *entity.User) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"name":  user.Name,
+		"email": user.Email,
+		"exp":   time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	tokenString, err := token.SignedString(secretKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func VerifyToken(tokenString string) error {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if !token.Valid {
+		return fmt.Errorf("invalid token")
+	}
+
+	return nil
+}
