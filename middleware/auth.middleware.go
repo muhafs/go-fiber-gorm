13bd5f36754f8411a1965f6f97ed8dbe70979ca2
Muhamad <muhamadhaspin@gmail.com>
2024-01-26 13:51:48 +0700

    implement verify token on auth middleware

--- middleware/auth.middleware.go
+++ middleware/auth.middleware.go
@@ -11,15 +11,21 @@ func Auth(c *fiber.Ctx) error {
 	// get Header's value
 	auth := c.Get("Authorization")
 
-	// validate token format
-	tokens := strings.Split(auth, " ")
-	if len(tokens) != 2 || tokens[0] != "Token" {
-		return utils.ErrorJSON(c, fiber.StatusBadRequest, "invalid token")
+	// split type and token from auth string
+	splittedAuth := strings.Split(auth, " ") // ["Bearer", "secrettokenstring123"]
+
+	// validate auth string format
+	if len(splittedAuth) != 2 {
+		return utils.ErrorJSON(c, fiber.StatusUnauthorized, "missing authorization header")
 	}
 
-	// validate token
-	if tokens[1] != "secret" {
-		return utils.ErrorJSON(c, fiber.StatusUnauthorized, "unauthorized")
+	// extract type and token
+	hType := splittedAuth[0]  // "Bearer"
+	hToken := splittedAuth[1] // "secrettokenstring123"
+
+	// validate token string
+	if _, err := utils.VerifyToken(hToken); err != nil || hType != "Bearer" {
+		return utils.ErrorJSON(c, fiber.StatusUnauthorized, "invalid token")
 	}
 
 	// pass to next
