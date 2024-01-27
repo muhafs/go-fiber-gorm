c786a47ceaf210126ab805cde9de71bd728a902d
Muhamad <muhamadhaspin@gmail.com>
2024-01-27 11:38:57 +0700

    set user avatar as nullable coulmn

--- model/entity/user.enity.go
+++ model/entity/user.enity.go
@@ -1,6 +1,7 @@
 package entity
 
 import (
+	"database/sql"
 	"time"
 
 	"gorm.io/gorm"
@@ -8,12 +9,13 @@ import (
 
 // model for storing into database
 type User struct {
-	ID        uint      `json:"id" gorm:"primaryKey"`
-	Name      string    `json:"name" gorm:"not null"`
-	Email     string    `json:"email" gorm:"not null;unique"`
-	Password  string    `json:"password" gorm:"not null"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        uint           `json:"id" gorm:"primaryKey"`
+	Name      string         `json:"name" gorm:"not null"`
+	Email     string         `json:"email" gorm:"not null;unique"`
+	Password  string         `json:"-" gorm:"not null"`
+	Avatar    sql.NullString `json:"avatar"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
 }
 
 func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
